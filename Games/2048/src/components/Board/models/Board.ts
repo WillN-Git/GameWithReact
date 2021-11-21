dc98fa1a53473dcef9e8f9da727f0d46fe48b4e2
Will_N <65072093+WillN-Git@users.noreply.github.com>
2021-11-22 00:33:15 +0100

    Create Board.ts

---
+++ Games/2048/src/components/Board/models/Board.ts
@@ -0,0 +1,16 @@
+import { pixelSize } from "../../../styles";
+
+/**
+ * The number of tiles per row and column.
+ */
+export const tileCount = 4;
+
+/**
+ * The duration of every animation in ms.
+ */
+export const animationDuration = 250;
+
+/**
+ * The board margin (left, right, top, bottom).
+ */
+export const boardMargin = 2 * pixelSize;
